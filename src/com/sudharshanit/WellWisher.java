d7f5e11babebbe41de700d86202c3eef57339fb2
Sudharshan-IT <sudharshanpingme@gmail.com>
2021-09-20 15:04:02 +0530

    Lamda Implementation is added in WellWisher Class

--- src/com/sudharshanit/WellWisher.java
+++ src/com/sudharshanit/WellWisher.java
@@ -7,31 +7,36 @@ public class WellWisher {
 
 	public static void main(String[] args) {
 		//AnnonyMours Implementation
+		/*
 		wish(new Greeting() {
-
 			@Override
 			public void greet() {
 				System.out.println("Hindi:** Namaskar **");
-				
 			}
-			
 		});
 		wish(new Greeting() {
-
 			@Override
 			public void greet() {
 			System.out.println("Telugu: ** Namaskaram");
-				
 			}
-			
 		});
 		wish(new Greeting() {
-
 			@Override
 			public void greet() {
 				System.out.println("English: ** Good Moring **");
-				
 			}
+			
+		});
+		*/
+		//Using Lamda Expression
+		wish(()->{
+			System.out.println("Namaste...!");
+		});
+		wish(()->{
+			System.out.println("Good Moring...!");
+		});
+		wish(()->{
+			System.out.println("Namaskaram");
 			
 		});
 		
